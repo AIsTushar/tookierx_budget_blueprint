678b95d9d1a9973b78c589c1ac2b972b48fd8366
AIsTushar <azizultushar98@gmail.com>
2025-11-08 11:06:44 +0600

    imporved the file structure

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,12 +2,8 @@ import { User } from "@prisma/client";
 import ApiError from "../../error/ApiErrors";
 import { StatusCodes } from "http-status-codes";
 import { compare, hash } from "bcrypt";
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { OTPFn } from "../../helper/OTPFn";
-import OTPVerify from "../../helper/OTPVerify";
-import { getImageUrl } from "../../helper/uploadFile";
 import { prisma } from "../../../utils/prisma";
-import { jwtHelpers } from "../../helper/jwtHelper";
 
 const createUserIntoDB = async (payload: User) => {
   const findUser = await prisma.user.findUnique({
@@ -51,17 +47,14 @@ const changePasswordIntoDB = async (id: string, payload: any) => {
     },
     select: {
       password: true,
-    }
+    },
   });
   if (!findUser) {
     throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
   }
   const comparePassword = await compare(payload.oldPassword, findUser.password);
   if (!comparePassword) {
-    throw new ApiError(
-      StatusCodes.UNAUTHORIZED,
-      "Invalid password"
-    );
+    throw new ApiError(StatusCodes.UNAUTHORIZED, "Invalid password");
   }
 
   const hashedPassword = await hash(payload.newPassword, 10);
@@ -77,8 +70,6 @@ const changePasswordIntoDB = async (id: string, payload: any) => {
 };
 
 const updateUserIntoDB = async (id: string, payload: any, image: any) => {
-  const userImage = image && await getImageUrl(image);
-
   const findUser = await prisma.user.findUnique({
     where: {
       id,
@@ -94,7 +85,6 @@ const updateUserIntoDB = async (id: string, payload: any, image: any) => {
     },
     data: {
       ...payload,
-      image: userImage ?? undefined,
     },
     select: {
       id: true,
